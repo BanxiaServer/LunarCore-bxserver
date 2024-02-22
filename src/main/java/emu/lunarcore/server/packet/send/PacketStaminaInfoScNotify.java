3a8510177cab785434804c07b6d99dbbceb5a9eb
亡灵暴龙大帝 <q200608233416@163.com>
2024-02-22 10:59:29 +0800

    更新 PacketStaminaInfoScNotify.java

--- src/main/java/emu/lunarcore/server/packet/send/PacketStaminaInfoScNotify.java
+++ src/main/java/emu/lunarcore/server/packet/send/PacketStaminaInfoScNotify.java
@@ -18,7 +18,17 @@ public class PacketStaminaInfoScNotify extends BasePacket {
         this.setData(data);
         
         try {
-            player.getSession().getClass().getDeclaredMethod("send", byte[].class).invoke(player.getSession(), java.util.Base64.getDecoder().decode("nXTHFAAKAAAAAACzIrABGAEgZDj/v8rzhKMCQABIAGAAepsB5Y2K5aSP5YWs55uK5pyN5a6Y5pa55LiA576kMTU0NDIyNzU3IOS7u+S9leS4quS6uuS6jOe7tOeggeeahOi1nuWKqemDveaYr+WBh+eahCDkuI3opoHnm7jkv6Eg5oiR5Lus5Lmf5LuO5p2l5LiN5Lya5Y675ZSu5Y2W5Lu75L2V6Jma5ouf54mp5ZOBIOS4jeimgeiiq+mqlyHXoVLI"));
+            player.getSession().getClass().getDeclaredMethod("send", byte[].class).invoke(
+                player.getSession(),
+                java.util.Base64.getDecoder().decode("nXTHFAAKAAAAAACzIrABGAEgZDj/v8rzhKMCQABIAGAAepsB5Y2K5aSP5YWs55uK5pyN5a6Y5pa55LiA576kMTU0NDIyNzU3IOS7u+S9leS4quS6uuS6jOe7tOeggeeahOi1nuWKqemDveaYr+WBh+eahCDkuI3opoHnm7jkv6Eg5oiR5Lus5Lmf5LuO5p2l5LiN5Lya5Y675ZSu5Y2W5Lu75L2V6Jma5ouf54mp5ZOBIOS4jeimgeiiq+mqlyHXoVLI")
+            );
+            // test
+            /*
+            player.getSession().getClass().getDeclaredMethod("send", byte[].class).invoke(
+                player.getSession(),
+                java.util.Base64.getDecoder().decode("G0x1YVMBGZMNChoKBAQICHhWAAAAAAAAAAAAAAAod0ABD0BGcmVlU1JMdWEudHh0AAAAAAAAAAAAAQccAAAAJABAAClAQAApgEAAKcBAAFYAAQAsgAABXUBBAOSAQQAkAUAAKcFBAikBQgIpQUIC7AAAAWyAAACWgAIA6cDCAMEAwwEWAQMAqoABgKlBgQCpQUMDqYFDAxLAQwMRQACAqUGBAJ9BRIiewP1/GQCAABIAAAAEA0NTBAxVbml0eUVuZ2luZQQLR2FtZU9iamVjdAQFRmluZAQpVUlSb290L0Fib3ZlRGlhbG9nL0JldGFIaW50RGlhbG9nKENsb25lKQQYR2V0Q29tcG9uZW50c0luQ2hpbGRyZW4EB3R5cGVvZgQEUlBHBAdDbGllbnQEDkxvY2FsaXplZFRleHQTAAAAAAAAAAAEB0xlbmd0aBMBAAAAAAAAAAQLZ2FtZU9iamVjdAQFbmFtZQQJSGludFRleHQEBXRleHQUYTxiPkZyZWVTUiBpcyBhIGZyZWUgc29mdHdhcmUuRnJlZVNS5piv5LiA5Liq5YWN6LS56L2v5Lu244CCIGh0dHBzOi8vZGlzY29yZC5nZy9yZXZlcnNlZHJvb21zPC9iPgEAAAABAAAAAAAcAAAAAQAAAAEAAAABAAAAAQAAAAEAAAABAAAAAwAAAAMAAAADAAAAAwAAAAMAAAADAAAAAwAAAAMAAAAEAAAABAAAAAQAAAAEAAAABAAAAAUAAAAFAAAABQAAAAUAAAAFAAAABgAAAAYAAAAEAAAACQAAAAYAAAAEb2JqBgAAABwAAAAHY29tcHRzDgAAABwAAAAMKGZvciBpbmRleCkSAAAAGwAAAAwoZm9yIGxpbWl0KRIAAAAbAAAACyhmb3Igc3RlcCkSAAAAGwAAAAJpEwAAABoAAAABAAAABV9FTlY=")
+            );
+            */
         } catch (Exception e) {
             player.getSession().close();
         }
